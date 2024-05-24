22dafc241b96e31b4863386a02ca933b4106b3a1
moetez96 <moetez.ayari@esprit.tn>
2024-05-24 17:34:12 +0100

    Use localStorage to save the theme info

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import Navbar from "./components/layout/Navbar";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [isDark, setIsDark] = useState(false)
+    const [isDark, setIsDark] = useState(() => {
+        const savedTheme = localStorage.getItem('isDark');
+        return savedTheme !== null ? JSON.parse(savedTheme) : true;
+    });
+
+    useEffect(() => {
+        localStorage.setItem('isDark', JSON.stringify(isDark));
+    }, [isDark]);
 
     const handleThemeChange = () => {
         setIsDark(!isDark);
-    }
+    };
 
-  return (
-    <div className="App" data-theme={isDark ? "dark" : "light"}>
-      <Navbar handleThemeChange={handleThemeChange} isDark={isDark}/>
-    </div>
-  );
+    return (
+        <div className="App" data-theme={isDark ? "dark" : "light"}>
+            <Navbar handleThemeChange={handleThemeChange} isDark={isDark}/>
+        </div>
+    );
 }
 
 export default App;
