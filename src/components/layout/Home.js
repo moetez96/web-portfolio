55ce345deffd1a425f698f52f54d17c24b4f5f97
moetez96 <moetez.ayari@esprit.tn>
2024-05-26 23:19:55 +0100

    Add Home section responsive style

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -9,12 +9,15 @@ function Home() {
                     <h1>hi, i am</h1>
                     <h1>Moetez Ayari.</h1>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.                    </p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
                     <span className="profile-contact-btns">
                         <button>
                             Contact Me
                             <div className="circle">
-                                <span><ArrowOutward /></span>
+                                <span>
+                                    <ArrowOutward fontSize={'medium'}/>
+                                </span>
                             </div>
                         </button>
                         <div className="contact-icons">
