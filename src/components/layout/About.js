3ae5c3cb910f30c1bc4ff27c14cec2a9d43db37e
moetez96 <moetez.ayari@esprit.tn>
2024-05-30 16:18:24 +0100

    Add skills section

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -10,8 +10,19 @@ function About() {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </div>
+            <div className="about-skills-container">
+                <div className="about-skills">
+                    SKILLS
+                </div>
+                <div className="about-skills-list">
+                    {placeHolederSkills.map((elem) => (<span className="skill-chip">{elem}</span>))}
+                </div>
+            </div>
         </div>
     );
 }
 
 export default About;
+
+
+const placeHolederSkills = ["C","C--","C++","C#","C/AL","Caché ObjectScript","C Shell","Caml","Candle","Cayenne","CDuce","Cecil","Cel","Cesil","Ceylon","CFEngine","CFML","Cg","Ch","Chapel","CHAIN","Charity","Charm","Chef","CHILL","CHIP-8","chomski","ChucK","CICS","Cilk","CL","Claire","Clarion","Clean","Clipper","CLIST","Clojure","CLU","CMS-2","COBOL","Cobra","CODE","CoffeeScript","Cola","ColdC","ColdFusion","COMAL","Combined Programming Language", "Constraint Handling Rules","Converge","Cool","Coq","Coral 66","Corn","CorVision","COWSEL","CPL","csh","CSP","Csound","CUDA","Curl","Curry","Cyclone","Cython","D","DASL","DASL","Dart","DataFlex","Datalog","DATATRIEVE"];
