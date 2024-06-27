e988cdc1d3dea3b77a6f5a8e925e1774b9afff19
moetez96 <moetez.ayari@esprit.tn>
2024-06-27 12:51:15 +0100

    Add pagination to the experience card

--- src/components/cards/ExperienceCard.js
+++ src/components/cards/ExperienceCard.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useSpring, animated, useSprings } from "react-spring";
 import { useInView } from "react-intersection-observer";
-import {easeCubicIn} from "d3-ease";
+import { easeCubicIn } from "d3-ease";
 
 function ExperienceCard({ exp, index }) {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
     const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+    const [selectIndex, setSelectIndex] = useState(0);
+    const [selectIndexContent, setSelectIndexContent] = useState(0);
+    const [isSlidingOut, setIsSlidingOut] = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
@@ -39,6 +42,25 @@ function ExperienceCard({ exp, index }) {
         delay: index * 400
     }));
 
+    const handlePaginationClick = (index) => {
+        if (index !== selectIndex) {
+            setIsSlidingOut(true);
+            setSelectIndex(index);
+            setTimeout(() => {
+                setSelectIndexContent(index);
+                setIsSlidingOut(false);
+            }, 300);
+        }
+    };
+
+    const slideAnimation = useSpring({
+        opacity: isSlidingOut ? 0 : 1,
+        transform: isSlidingOut ?
+            selectIndex % 2 === 0 ? 'translateX(20px)': 'translateX(-20px)'  :
+            selectIndex % 2 === 0 ? 'translateX(0)' : 'translateX(0)',
+        config: { duration: 300 }
+    });
+
     return (
         <animated.div
             className={`timeline-item ${isMobile ? 'right' : index % 2 === 0 ? 'left' : 'right'}`}
@@ -46,16 +68,28 @@ function ExperienceCard({ exp, index }) {
             ref={ref}
             style={animationProps}
         >
-            <div className="timeline-item-content">
+
+            <div className="timeline-item-content" style={{ opacity: animationProps.opacity }}>
+                {exp.projects.length > 1 && (
+                    <div className="pagination-indicators">
+                        {exp.projects.map((elem, idx) => (
+                            <span
+                                key={idx}
+                                className={`pagination-circle ${idx === selectIndex ? 'active' : ''}`}
+                                onClick={() => handlePaginationClick(idx)}
+                            ></span>
+                        ))}
+                    </div>
+                )}
                 <animated.span className="tag">{exp.date}</animated.span>
-                <animated.h3 style={{ opacity: animationProps.opacity }}>{exp.title}</animated.h3>
-                <animated.p style={{ opacity: animationProps.opacity }}>{exp.description}</animated.p>
+                <animated.h3>{exp.title + " - " }<animated.span style={slideAnimation}>{exp.projects[selectIndexContent].role}</animated.span></animated.h3>
+                <animated.p style={slideAnimation}>{exp.projects[selectIndexContent].description}</animated.p>
                 {springs.map((props, i) => (
                     <animated.span key={i} className={`circle ${index === 0 ? 'active' : ''}`}
                                    style={index === 0 ? { transform: props.scale.to(s => `scale(${s})`), opacity: props.opacity } : {}} />
                 ))}
                 <div className="tech">
-                    <p>{' '} {exp.technologies}</p>
+                    <animated.p style={slideAnimation}>{exp.projects[selectIndexContent].technologies}</animated.p>
                 </div>
             </div>
         </animated.div>
--- src/components/layout/Experience.js
+++ src/components/layout/Experience.js
@@ -8,34 +8,70 @@ import ExperienceCard from "../cards/ExperienceCard";
 function Experience() {
     const experiences = [
         {
+            title: 'Deloitte Tunisia',
             date: 'October 2022 – Present',
-            title: 'Deloitte Tunisia - Full Stack Developer',
-            description: 'I participated in developing a BMW application focused on damage management during car transportation, contributing to the migration from monolithic to microservices architecture while gaining insights into best practices. I also played a role in enhancing the user interface to improve system functionality and user experience, gaining valuable experience in both backend and frontend development within a collaborative team setting.',
-            technologies: ' Angular, SpringBoot, Java 11, PostgreSQL, Docker, Jenkins, AWS S3.'
+            projects: [
+                {
+                    projectName: 'BMW Damage Management Application',
+                    role: 'Full Stack Developer',
+                    technologies: 'Angular, SpringBoot, Java 11, PostgreSQL, Docker, Jenkins, AWS S3',
+                    description: 'I participated in developing a BMW application focused on damage management during car transportation, contributing to the migration from monolithic to microservices architecture while gaining insights into best practices. I also played a role in enhancing the user interface to improve system functionality and user experience, gaining valuable experience in both backend and frontend development within a collaborative team setting.'
+                },
+                {
+                    projectName: 'Tunisie Telecom BSCS',
+                    role: 'Software Developer',
+                    technologies: 'J2EE, SpringBoot, Java 11, PostgreSQL, Docker, Jenkins',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                }
+            ]
         },
         {
-            date: ' February 2022- August 2022',
-            title: 'Orange Digital Center - End of Study Internship',
-            description: 'Development of a candidates sourcing website that aims to offer enterprises a more fleshed out image of their potential candidates, and opennew opportunities for candidates to better reach new recruiters.',
-            technologies: 'Node.js, React.js, GitHub, MongoDB, Next.js, Express.js.'
+            title: 'Orange Digital Center',
+            date: 'February 2022- August 2022',
+            projects: [
+                {
+                    projectName: 'Candidates Sourcing Website',
+                    role: 'End of Study Internship',
+                    technologies: 'Node.js, React.js, GitHub, MongoDB, Next.js, Express.js',
+                    description: 'Development of a candidates sourcing website that aims to offer enterprises a more fleshed out image of their potential candidates, and open new opportunities for candidates to better reach new recruiters.'
+                }
+            ]
         },
         {
+            title: 'ESPRIT TECH RDI',
             date: 'July 2021- August 2021',
-            title: 'ESPRIT TECH RDI - Summer Internship',
-            description: 'Development of a personalized E-Learning platform that aims to provide users with specific courses related to their fields of specialization',
-            technologies: 'MongoDB, Spring Boot, Angular.'
+            projects: [
+                {
+                    projectName: 'Personalized E-Learning Platform',
+                    role: 'Summer Internship',
+                    technologies: 'MongoDB, Spring Boot, Angular',
+                    description: 'Development of a personalized E-Learning platform that aims to provide users with specific courses related to their fields of specialization.'
+                }
+            ]
         },
         {
+            title: 'ISI KEF',
             date: 'January 2019 – June 2019',
-            title: 'ISI KEF - End of Study Internship',
-            description: 'Development of a mobile application for university events, courses and forums.',
-            technologies: 'Android Studio, Firebase.'
+            projects: [
+                {
+                    projectName: 'University Events Mobile Application',
+                    role: 'End of Study Internship',
+                    technologies: 'Android Studio, Firebase',
+                    description: 'Development of a mobile application for university events, courses and forums.'
+                }
+            ]
         },
         {
+            title: 'Cryptec',
             date: 'June 2018 – August 2018',
-            title: 'Cryptec - Summer Internship',
-            description: 'Development of a management Desktop application for employees schedules and machines dysfunctions in the workspace.',
-            technologies: 'VB.NET, SQL.'
+            projects: [
+                {
+                    projectName: 'Management Desktop Application',
+                    role: 'Summer Internship',
+                    technologies: 'VB.NET, SQL',
+                    description: 'Development of a management Desktop application for employees schedules and machines dysfunctions in the workspace.'
+                }
+            ]
         }
     ];
 
