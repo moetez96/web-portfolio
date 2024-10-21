694ebb37d27915ce7b29a7fd902aa304c0713761
moetez96 <moetez.ayari@esprit.tn>
2024-10-21 16:13:23 +0100

    Remove unnecessary useEffect from home and project pages

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -20,6 +20,10 @@ function Project() {
     const section1Animation = useSection1Animation();
 
     useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    useEffect(() => {
         if (name) {
             const project = projects.list.find((proj) => proj.name === name);
             const projectInfo = resume.projects.list.find((proj) => proj.name === name);
